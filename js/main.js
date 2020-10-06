a07c0f7891b3dcda02efda38b1362f974e0a5219
Giovane Costa Silva <giovane@unir.br>
2020-10-06 00:30:38 -0400

    aula: introdução ao javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Giovane Costa";
+var n1 = "37";
+var n2 = "10";
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+console.log(nome);
+//console.log(idade + idade2);
+console.log(frase.toLowerCase());
+console.log(frase.replace("Japão", "Brasil"));
+console.log(n1 * n2);

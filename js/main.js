c61e93e0d9fa5509cf576c0141f72ceb5e77c846
Giovane Costa Silva <giovane@unir.br>
2020-10-06 01:01:16 -0400

    aula: array e dicionário

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
-var nome = "Giovane Costa";
-var n1 = "37";
-var n2 = "10";
-var frase = "Japão é o melhor time do mundo";
+var frutas = [
+  { nome: "maça", cor: "vermelha" },
+  { nome: "uva", cor: "roxa" },
+];
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*
+var fruta = { nome: "maça", cor: "vermelha" };
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//var nome = "Giovane Costa";
+//var n1 = "37";
+//var n2 = "10";
+//var frase = "Japão é o melhor time do mundo";
 //alert(nome + " tem " + idade + " anos.");
 //alert(idade + idade2);
-console.log(nome);
-//console.log(idade + idade2);
-console.log(frase.toLowerCase());
-console.log(frase.replace("Japão", "Brasil"));
-console.log(n1 * n2);
+//console.log(nome);
+//console.log(frase.toLowerCase());
+//console.log(frase.replace("Japão", "Brasil"));
+//console.log(n1 * n2);
